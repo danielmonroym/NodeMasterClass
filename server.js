3c17e8ed9db56f80211e67d1bfdf6ea4a271c86f
daniel.monroy <daniel.monroym@udea.edu.co>
2020-12-22 09:59:42 -0500

    Express router

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
-const express= require('express');
-const dotenv= require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
 
+//Route files
+const bootcamps = require('./routes/bootcamps');
 // Load .env vars
 dotenv.config({
-    path: './config/config.env'
+  path: "./config/config.env",
 });
 
 const app = express();
 
-const PORT= process.env.PORT || 5000;
+//app.get("/", (req, res) => {
+  //Cambiar status
+  // res.status(400).send('Error');
+  //res.send("Hello from express");
+//});
 
-app.listen(PORT, console.log(`Server runnig in ${process.env.NODE_ENV} mode on port ${PORT}`));
+// Mount routers
+
+app.use('/api/v1/bootcamps', bootcamps);
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
